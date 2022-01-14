518ee198228bcfeb91fc0eebad53766c2a79fd40
pavelloi <pashaloy95@gmail.com>
2022-01-15 00:16:41 +0200

    10th task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+input.addEventListener('input', event => {
+  input.setAttribute('counter', event.currentTarget.value);
+});
+
+
+const onCreateBoxes = () => {
+  let boxSize = 30;
+  let allDivsEl = [];
+  for (let i = 1; i <= input.getAttribute('counter'); i += 1) {
+    boxSize += 10;
+    const newDiv = document.createElement('div');
+    newDiv.classList.add("box-item");
+
+    newDiv.style.width = `${boxSize}px`;
+    newDiv.style.height = `${boxSize}px`;
+
+    newDiv.style.border = '2px solid black';
+    newDiv.style.backgroundColor = `${getRandomHexColor()}`;
+
+    allDivsEl.push(newDiv);
+  }
+  boxes.append(...allDivsEl);
+};
+
+createBtn.addEventListener('click', onCreateBoxes);
+
+destroyBtn.addEventListener('click', () => {
+  boxes.innerHTML = '';
+  input.removeAttribute('counter');
+  input.value = '';
+});
