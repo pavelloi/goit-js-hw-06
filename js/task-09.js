85c7a491fe618dd69b26034d659df70ed53a8f5c
pavelloi <pashaloy95@gmail.com>
2022-01-14 23:08:14 +0200

    9th task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+
+const colorBtn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+colorBtn.addEventListener("click", event => {
+  span.textContent = getRandomHexColor();
+  if (event.currentTarget.classList.contains("change-color")) {
+    document.body.style.background = getRandomHexColor()
+  }
+})
