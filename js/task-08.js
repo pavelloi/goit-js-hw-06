c2d7df248b1e782be1175640fc40641683f6793b
pavelloi <pashaloy95@gmail.com>
2022-01-14 22:32:43 +0200

    8th task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+    event.preventDefault(); // для НЕ перезапуска странички //
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = { email, password };
+    
+    if (email.value === "" || password.value === "") {
+        alert('All fields should be filled by you');
+    } else {
+        console.log(formData);
+    }
+    form.reset();
+}
