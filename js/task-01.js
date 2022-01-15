25be16198028d8fc90bf5481c473151c7b36601f
pavelloi <pashaloy95@gmail.com>
2022-01-15 23:34:46 +0200

    updated 1st task

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,15 @@
 const list = document.querySelectorAll('.item');
 console.log(`Number of categories: `, list.length);
 
-const categoryTitle = document.querySelectorAll('h2');
+// const categoryTitle = document.querySelectorAll('h2');
+const allCategories = document.querySelectorAll('ul#categories li.item')
 
-categoryTitle.forEach(title => {
-    console.log('Category: ', title.textContent);
-    const itemsList = title.nextElementSibling;
-    const items = itemsList.children;
-    console.log('Elements: ', items.length);
+allCategories.forEach(title => {
+    console.log('Category: ', title.firstElementChild.textContent);
+    console.log('Elements: ', title.lastElementChild.children.length);
+    // const itemsList = title.nextElementSibling;
+    // const items = itemsList.children;
+    // console.log('Elements: ', items.length);
  })
 
 
